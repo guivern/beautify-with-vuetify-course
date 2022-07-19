c8e0644ce30c43d65cf4dd7885e3e5ce7282c5a5
Guillermo Verón <guiver95@gmail.com>
2022-07-19 16:20:31 -0400

    form login components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <v-app>
     <v-main>
-      <v-card>
-        <v-card-title><h1>Hello world</h1></v-card-title>
-        <v-card-text>Text</v-card-text>
-        <v-btn color="primary">Button</v-btn>
+      <v-card width="400px" class="mx-auto mt-5">
+        <v-card-title class="pb-4">
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Username"
+              prepend-icon="mdi-account-circle"
+            ></v-text-field>
+            <v-text-field
+              label="Password"
+              prepend-icon="mdi-lock"
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
       </v-card>
     </v-main>
   </v-app>
@@ -17,7 +38,7 @@ export default {
   components: {},
 
   data: () => ({
-    //
+    showPassword: false,
   }),
 };
 </script>
